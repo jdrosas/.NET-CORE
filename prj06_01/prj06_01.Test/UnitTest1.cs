c119e2924d1280e58737713e5e9f1ddeb8eae693
jdrosas <juandiazjdrd@gmail.com>
2023-08-13 22:48:24 -0500

    Clase 20230805

---
+++ prj06_01/prj06_01.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace prj06_01.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ prj06_01/prj06_01/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prj06_01.Models;
+using System.Diagnostics;
+
+namespace prj06_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
